b05b1c6c7aa143ebd2720e4c60ae7057eb2c2ed9
ArtemAntoshchenko <fgd19611@yndex.ru>
2025-11-18 17:30:28 +0300

    update main.js

--- main.js
+++ main.js
@@ -5,6 +5,20 @@ async function getCars(){
         const rows=document.querySelector('tbody')
         cars.forEach(car=>rows.append(row(car)))
     }
-        
+}
+function row(car){
+    const tr=document.createElement('tr')
+    tr.setAttribute('data-rowid', car.id)
 
+    const nameTd=document.createElement('td')
+    nameTd.append(car.name)
+    tr.append(nameTd)
+
+    const markTd=document.createElement('td')
+    markTd.append(car.mark)
+    tr.append(markTd)
+
+    const colorTd=document.createElement('td')
+    colorTd.append(car.color)
+    tr.append(colorTd)
 }
