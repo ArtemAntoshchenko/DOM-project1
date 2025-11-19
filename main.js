0b08639694eb5b2d846085e363fa14b2d2933528
ArtemAntoshchenko <fgd19611@yndex.ru>
2025-11-19 16:49:16 +0300

    update main.py, main.js

--- main.js
+++ main.js
@@ -6,6 +6,35 @@ async function getCars(){
         cars.forEach(car=>rows.append(row(car)))
     }
 }
+
+async function getCar(id){
+    const respons=await fetch(`/cars/${id}`, {method:'GET', headers:{'Accept':'application/json'}})
+    if (respons.ok===true){
+        const car=await respons.json()
+        document.getElementById('carId').value=car.id
+        document.getElementById('carName').value=car.name
+        document.getElementById('carMark').value=car.mark
+        document.getElementById('carColor').value=car.color
+    }
+    else {
+        const error=await respons.json()
+        console.log(error.message)
+    }
+    
+}
+
+async function createCar(carName,carMark,carColor){
+    const respons=await fetch('cars', {
+        method:'Get', 
+        headers:{'Accept':'application/json'}, 
+        body:JSON.stringify({name:carName, mark:carMark, clolor:carColor})
+    }) 
+    if (respons.ok===true){
+        const car=await respons.json()
+        document.querySelector('tbody').append(row(car))
+    }
+}
+
 function row(car){
     const tr=document.createElement('tr')
     tr.setAttribute('data-rowid', car.id)
@@ -21,4 +50,15 @@ function row(car){
     const colorTd=document.createElement('td')
     colorTd.append(car.color)
     tr.append(colorTd)
+
+    const linksTd=document.createElement('td')
+
+    const buyLink=document.createElement('button')
+    buyLink.append('Купить автомобиль')
+    buyLink.addEventListener('click', async()=>await getCar(car.id))
+
+    linksTd.append(buyLink)
+    tr.appendChild(linksTd)
+    return tr
+
 }
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Depends, Body
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
 from sqlalchemy.orm import Session
 from database import *
 
@@ -22,6 +22,13 @@ def index():
 def get_cars(db: Session=Depends(db_get)):
     return db.query(Cars).all()
 
+@app.get('cars/{id}')
+def get_car(id,db: Session=Depends(db_get)):
+    car=db.query(Cars).filter(Cars.id==id).first()
+    if car==None:
+        return JSONResponse(status_code=404, content={'message':'Автомомбиль не найден'})
+    return car
+
 @app.post('/add')
 def add_car(data=Body(), db: Session=Depends(db_get)):
     new_car=Cars(name=data['name'], mark=data['mark'], color=data['color'])
@@ -29,3 +36,12 @@ def add_car(data=Body(), db: Session=Depends(db_get)):
     db.commit()
     db.refresh(new_car)
     return new_car
+
+@app.delete('/cars/{id}')
+def delet_car(id,db: Session=Depends(db_get)):
+    del_car=db.query(Cars).filter(Cars.id==id).first()
+    if del_car==None:
+        return JSONResponse(status_code=404, content={'message':'Автомомбиль не найден'})
+    db.delete(del_car)
+    db.commit()
+    return del_car
